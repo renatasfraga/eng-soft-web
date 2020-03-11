36049409b48c761e2ccc2d62141324b107ffeb57
Renata Fraga <renatasfraga@edu.unisinos.br>
2020-03-10 23:40:05 -0300

    :tada: backend project structure

---
+++ backend/bem-api/src/test/java/edu/unisinos/bemapi/BemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unisinos.bemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
